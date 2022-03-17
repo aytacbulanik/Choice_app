10ad9def3a92946890eed32f8117e23148f23991
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-03-17 21:30:05 +0300

    Initial Commit

---
+++ Choice_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Choice_app
+//
+//  Created by AYTAÇ BULANIK on 17.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
